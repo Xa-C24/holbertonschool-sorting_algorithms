4daf48791d3e31504aad496fc9a18b7e11d5fbe4
Xa-C24 <9335@holbertonstudents.com>
2024-08-09 09:25:44 +0200

    modification

--- sort.h
+++ sort.h
@@ -19,17 +19,13 @@
 		struct listint_s *next;
 	} listint_t;
 
+		void print_list(const listint_t *list);
+		void print_array(const int *array, size_t size);
+		/* Declaration of the function for printing the list*/
+
 		void bubble_sort(int *array, size_t size);
 		void insertion_sort_list(listint_t **list);
 		/* Declaration of the function for sorting the list.*/
-		void selection_sort(int *array, size_t size);
 		void quick_sort(int *array, size_t size);
-
-		void print_list(const listint_t *list);
-<<<<<<< HEAD
-		void print_array(const int *array, size_t size);
-=======
-		/* Declaration of the function for printing the list*/
->>>>>>> c9aec365490d7610fc34b4a6a3bf70c1394af0fa
 
 #endif
